27ab9032cdbba238835c7ee9e4bddfb95e59e8b6
Mert Gökmen <mertgkmeen@gmail.com>
2022-06-10 17:35:47 +0300

    did it

--- backend/api/views.py
+++ backend/api/views.py
@@ -309,9 +309,151 @@ def AnketEkle(request):
     if request.method=="POST":
         serializer = AnketSoruSerializer(data = request.data)
         print(request.data)
-        if serializer.is_valid():
-            serializer.save()
+        AnketSoru.objects.create(
+            baslik=request.data['baslik'],
 
-            return Response(serializer.data)
+            soru1=request.data['soru1'],
+            soru2=request.data['soru2'],
+            soru3=request.data['soru3'],
+            soru4=request.data['soru4'],
+            soru5=request.data['soru5'],
+            soru6=request.data['soru6'],
+            soru7=request.data['soru7'],
+            soru8=request.data['soru8'],
+            soru9=request.data['soru9'],
+            soru10=request.data['soru10'],
+            soru11=request.data['soru11'],
+            soru12=request.data['soru12'],
+            soru13=request.data['soru13'],
+            soru14=request.data['soru14'],
+            soru15=request.data['soru15'],
+            soru16=request.data['soru16'],
+            soru17=request.data['soru17'],
+            soru18=request.data['soru18'],
+            soru19=request.data['soru19'],
+            soru20=request.data['soru20'],
+
+            soru1cevap1=request.data['soru1cevap1'],
+            soru1cevap2=request.data['soru1cevap2'],
+            soru1cevap3=request.data['soru1cevap3'],
+            soru1cevap4=request.data['soru1cevap4'],
+            soru1cevap5=request.data['soru1cevap5'],
+
+            soru2cevap1=request.data['soru2cevap1'],
+            soru2cevap2=request.data['soru2cevap2'],
+            soru2cevap3=request.data['soru2cevap3'],
+            soru2cevap4=request.data['soru2cevap4'],
+            soru2cevap5=request.data['soru2cevap5'],
+
+            soru3cevap1=request.data['soru3cevap1'],
+            soru3cevap2=request.data['soru3cevap2'],
+            soru3cevap3=request.data['soru3cevap3'],
+            soru3cevap4=request.data['soru3cevap4'],
+            soru3cevap5=request.data['soru3cevap5'],
+
+            soru4cevap1=request.data['soru4cevap1'],
+            soru4cevap2=request.data['soru4cevap2'],
+            soru4cevap3=request.data['soru4cevap3'],
+            soru4cevap4=request.data['soru4cevap4'],
+            soru4cevap5=request.data['soru4cevap5'],
+
+            soru5cevap1=request.data['soru5cevap1'],
+            soru5cevap2=request.data['soru5cevap2'],
+            soru5cevap3=request.data['soru5cevap3'],
+            soru5cevap4=request.data['soru5cevap4'],
+            soru5cevap5=request.data['soru5cevap5'],
+
+            soru6cevap1=request.data['soru6cevap1'],
+            soru6cevap2=request.data['soru6cevap2'],
+            soru6cevap3=request.data['soru6cevap3'],
+            soru6cevap4=request.data['soru6cevap4'],
+            soru6cevap5=request.data['soru6cevap5'],
+
+            soru7cevap1=request.data['soru7cevap1'],
+            soru7cevap2=request.data['soru7cevap2'],
+            soru7cevap3=request.data['soru7cevap3'],
+            soru7cevap4=request.data['soru7cevap4'],
+            soru7cevap5=request.data['soru7cevap5'],
+
+            soru8cevap1=request.data['soru8cevap1'],
+            soru8cevap2=request.data['soru8cevap2'],
+            soru8cevap3=request.data['soru8cevap3'],
+            soru8cevap4=request.data['soru8cevap4'],
+            soru8cevap5=request.data['soru8cevap5'],
+
+            soru9cevap1=request.data['soru9cevap1'],
+            soru9cevap2=request.data['soru9cevap2'],
+            soru9cevap3=request.data['soru9cevap3'],
+            soru9cevap4=request.data['soru9cevap4'],
+            soru9cevap5=request.data['soru9cevap5'],
+
+            soru10cevap1=request.data['soru10cevap1'],
+            soru10cevap2=request.data['soru10cevap2'],
+            soru10cevap3=request.data['soru10cevap3'],
+            soru10cevap4=request.data['soru10cevap4'],
+            soru10cevap5=request.data['soru10cevap5'],
+
+            soru11cevap1=request.data['soru11cevap1'],
+            soru11cevap2=request.data['soru11cevap2'],
+            soru11cevap3=request.data['soru11cevap3'],
+            soru11cevap4=request.data['soru11cevap4'],
+            soru11cevap5=request.data['soru11cevap5'],
+
+            soru12cevap1=request.data['soru12cevap1'],
+            soru12cevap2=request.data['soru12cevap2'],
+            soru12cevap3=request.data['soru12cevap3'],
+            soru12cevap4=request.data['soru12cevap4'],
+            soru12cevap5=request.data['soru12cevap5'],
+
+            soru13cevap1=request.data['soru13cevap1'],
+            soru13cevap2=request.data['soru13cevap2'],
+            soru13cevap3=request.data['soru13cevap3'],
+            soru13cevap4=request.data['soru13cevap4'],
+            soru13cevap5=request.data['soru13cevap5'],
+
+            soru14cevap1=request.data['soru14cevap1'],
+            soru14cevap2=request.data['soru14cevap2'],
+            soru14cevap3=request.data['soru14cevap3'],
+            soru14cevap4=request.data['soru14cevap4'],
+            soru14cevap5=request.data['soru14cevap5'],
+
+            soru15cevap1=request.data['soru15cevap1'],
+            soru15cevap2=request.data['soru15cevap2'],
+            soru15cevap3=request.data['soru15cevap3'],
+            soru15cevap4=request.data['soru15cevap4'],
+            soru15cevap5=request.data['soru15cevap5'],
+
+            soru16cevap1=request.data['soru16cevap1'],
+            soru16cevap2=request.data['soru16cevap2'],
+            soru16cevap3=request.data['soru16cevap3'],
+            soru16cevap4=request.data['soru16cevap4'],
+            soru16cevap5=request.data['soru16cevap5'],
+
+            soru17cevap1=request.data['soru17cevap1'],
+            soru17cevap2=request.data['soru17cevap2'],
+            soru17cevap3=request.data['soru17cevap3'],
+            soru17cevap4=request.data['soru17cevap4'],
+            soru17cevap5=request.data['soru17cevap5'],
+
+            soru18cevap1=request.data['soru18cevap1'],
+            soru18cevap2=request.data['soru18cevap2'],
+            soru18cevap3=request.data['soru18cevap3'],
+            soru18cevap4=request.data['soru18cevap4'],
+            soru18cevap5=request.data['soru18cevap5'],
+
+            soru19cevap1=request.data['soru19cevap1'],
+            soru19cevap2=request.data['soru19cevap2'],
+            soru19cevap3=request.data['soru19cevap3'],
+            soru19cevap4=request.data['soru19cevap4'],
+            soru19cevap5=request.data['soru19cevap5'],
+
+            soru20cevap1=request.data['soru20cevap1'],
+            soru20cevap2=request.data['soru20cevap2'],
+            soru20cevap3=request.data['soru20cevap3'],
+            soru20cevap4=request.data['soru20cevap4'],
+            soru20cevap5=request.data['soru20cevap5'],
+
+        )
+        
 
     return Response("Enter data!")
